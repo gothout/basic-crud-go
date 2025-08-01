c549b57772313f8e07ede4faf45c147fbdcdb348
Lucas Chaves <54557869+gothout@users.noreply.github.com>
2025-08-01 17:29:39 -0300

    refactor middleware initialization

--- internal/app/admin/enterprise/handler/v1/enterprise/enterprise.go
+++ internal/app/admin/enterprise/handler/v1/enterprise/enterprise.go
@@ -2,28 +2,27 @@ package enterprise
 
 import (
 	"basic-crud-go/internal/app/admin/enterprise/controller"
-	"basic-crud-go/internal/app/admin/enterprise/repository"
+	entRepo "basic-crud-go/internal/app/admin/enterprise/repository"
 	"basic-crud-go/internal/app/admin/enterprise/service"
 	"basic-crud-go/internal/infrastructure/db/postgres"
+	mw "basic-crud-go/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterEnterpriseRoutes(router *gin.RouterGroup) {
-	// Repository
-	repo := repository.NewRepositoryImpl(postgres.GetDB())
-	//Service
-	svc := service.NewEnterpriseService(repo)
-	//Controller
+func RegisterEnterpriseRoutes(router *gin.RouterGroup, authMW *mw.Auth) {
+	db := postgres.GetDB()
+	entRepository := entRepo.NewRepositoryImpl(db)
+	svc := service.NewEnterpriseService(entRepository)
 	ctrl := controller.NewEnterpriseController(svc)
 
 	group := router.Group("/")
 	{
-		group.POST("", ctrl.CreateEnterpriseHandler)
+		group.POST("", authMW.Handler("create-enterprise"), ctrl.CreateEnterpriseHandler)
 		group.GET("read", ctrl.ReadEnterprisesHandler)
 		group.GET("read/:cnpj", ctrl.ReadEnterpriseHandler)
-		group.PUT("", ctrl.UpdateEnterpriseHandler)
-		group.DELETE(":cnpj", ctrl.DeleteEnterpriseHandler)
+		group.PUT("", authMW.Handler("update-enterprise"), ctrl.UpdateEnterpriseHandler)
+		group.DELETE(":cnpj", authMW.Handler("delete-enterprise"), ctrl.DeleteEnterpriseHandler)
 	}
 
 }
